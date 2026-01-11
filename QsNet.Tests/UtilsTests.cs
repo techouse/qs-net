6e03483ba50d1274ce5ea75349fda7c8b62fc7fd
Klemen Tusar <techouse@gmail.com>
2026-01-11 23:41:30 +0000

    fix: add tests for overflow handling in Merge method with integer keys

--- QsNet.Tests/UtilsTests.cs
+++ QsNet.Tests/UtilsTests.cs
@@ -1405,6 +1405,52 @@ public class UtilsTests
     }
 
     [Fact]
+    public void Merge_OverflowTracksIntegerKeys()
+    {
+        var options = new DecodeOptions { ListLimit = 1 };
+        var overflow = Utils.CombineWithLimit(new List<object?> { "a" }, "b", options);
+
+        var target = new Dictionary<object, object?> { [5] = "x" };
+        var merged = Utils.Merge(target, overflow);
+        Utils.IsOverflow(merged).Should().BeTrue();
+
+        var appended = Utils.CombineWithLimit(merged, "c", options);
+        appended.Should()
+            .BeEquivalentTo(
+                new Dictionary<object, object?>
+                {
+                    ["0"] = "a",
+                    ["1"] = "b",
+                    [5] = "x",
+                    ["6"] = "c"
+                }
+            );
+    }
+
+    [Fact]
+    public void Merge_OverflowIgnoresNonCanonicalStringIndices()
+    {
+        var options = new DecodeOptions { ListLimit = 1 };
+        var overflow = Utils.CombineWithLimit(new List<object?> { "a" }, "b", options);
+
+        var target = new Dictionary<object, object?> { ["010"] = "x" };
+        var merged = Utils.Merge(target, overflow);
+        Utils.IsOverflow(merged).Should().BeTrue();
+
+        var appended = Utils.CombineWithLimit(merged, "c", options);
+        appended.Should()
+            .BeEquivalentTo(
+                new Dictionary<object, object?>
+                {
+                    ["0"] = "a",
+                    ["1"] = "b",
+                    ["010"] = "x",
+                    ["2"] = "c"
+                }
+            );
+    }
+
+    [Fact]
     public void InterpretNumericEntities_ReturnsInputUnchangedWhenThereAreNoEntities()
     {
         Utils.InterpretNumericEntities("hello world").Should().Be("hello world");
